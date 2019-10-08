66baae02741b59166eedeb1d4adca3c8b45378dd
bryanb213 <blancasbryan@gmail.com>
2019-10-08 12:25:02 -0500

    added hat route

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+//takes function takes any component and spits out new modified component
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 
-const MenuItem = ({title, imgUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imgUrl, size, history, match, linkUrl}) => (
+    <div className={`${size} menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
 
         <div className="background-image"
             style={{backgroundImage: `url(${imgUrl})`}}
@@ -15,4 +18,5 @@ const MenuItem = ({title, imgUrl, size}) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
+// with withrouter we now have access to history and other route props
