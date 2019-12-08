336e85422be2f586ab3678a6cd4f5d7add1ac579
bryanb213 <blancasbryan@gmail.com>
2019-12-08 16:41:24 -0600

    .

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,5 @@
-
+import { UserActionTypes } from './user.types';
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',//these strings should never change
+    type: UserActionTypes.SET_CURRENT_USER,//these strings should never change
     payload: user
 });
