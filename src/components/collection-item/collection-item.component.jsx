07d5017429783690f122718dfdb1018c7f9d6978
bryanb213 <blancasbryan@gmail.com>
2019-12-09 14:57:00 -0600

    styled button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import CustomButton from '../form-button/form-button.component';
 import './collection-item.style.scss';
 
 const CollectionItem = ({id, name, price, imageUrl}) => {
@@ -13,6 +14,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => {
                 <span className="name">{name}</span>
                 <span className="price">${price}</span>
             </div>
+            <CustomButton className="custom-button" inverted>Add to Cart</CustomButton>
         </div>
     );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -31,7 +31,7 @@ const Header = ({ currentUser, hidden }) => {
                 )}
                 <CartIcon/>
             </div>
-            { hidden ? null : <CartDropdown /> }
+            {  hidden ? null : <CartDropdown /> }
         </div>
     );
 }
