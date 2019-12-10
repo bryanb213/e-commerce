b7569215da1513f2da69c8d03f18a1a6d5c48741
bryanb213 <blancasbryan@gmail.com>
2019-12-10 14:09:59 -0600

    added selector functionality to icon & dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
 import './cart-icon.style.scss';
 
@@ -23,9 +24,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = ({ cart: {cartItems} }) => ({
-    itemCount: cartItems.reduce(
-        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
