88ded7541ad424cde6411b9458a301ff8f8c1e0b
bryanb213 <blancasbryan@gmail.com>
2020-01-16 12:07:38 -0600

    added netlify

---
+++ build/precache-manifest.acb9897dbefe6d1f8a2848ff3ff99f91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2e029e07efef00d2db487854bc87d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cc81cb21a76874b7348",
+    "url": "/static/css/main.23490b99.chunk.css"
+  },
+  {
+    "revision": "3e01b511f44bf080c931",
+    "url": "/static/js/2.42c31773.chunk.js"
+  },
+  {
+    "revision": "7613cbb6c1421ad0429f58fd99d1d7fe",
+    "url": "/static/js/2.42c31773.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cc81cb21a76874b7348",
+    "url": "/static/js/main.aeac984e.chunk.js"
+  },
+  {
+    "revision": "4bd2b3b4ecc325fa47b6",
+    "url": "/static/js/runtime-main.0aeca41e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
